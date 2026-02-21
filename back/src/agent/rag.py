4e84270806fb67e842db8ad3ce1545f4f1863f1f
OVERDOZZZE <faustdevs@gmail.com>
2026-02-20 23:19:43 -0600

    Frontend added

---
+++ back/src/agent/rag.py
@@ -0,0 +1,14 @@
+from src.user.models import CustomUser
+from src.interview.models import (
+    Agent,
+    Question,
+    Interview,
+    InterviewQA
+)
+
+def collect_metadata():
+    pass
+
+
+
+
