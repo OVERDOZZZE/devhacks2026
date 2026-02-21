a7f891a3e32ab93a1c590cf532e186381e78e7af
OVERDOZZZE <faustdevs@gmail.com>
2026-02-21 04:53:09 -0600

    MVP completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import NewInterview from './pages/NewInterview'
+import Session from './pages/Session'
 
 export default function App() {
   return (
@@ -13,7 +14,7 @@ export default function App() {
         <Route path="/register" element={<Register />} />      
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/interviews/new" element={<NewInterview />} />
-        <Route path="/interviews/:id/session" element={<h1>Session</h1>} />
+        <Route path="/interviews/:id/session" element={<Session />} />
         <Route path="/interviews/:id/results" element={<h1>Results</h1>} />
       </Routes>
     </BrowserRouter>
